50fba6b612c191d2500d1b877bdc4ebfca5fd202
rachman1 <rachmanarief717@gmail.com>
2024-01-03 19:09:50 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Fungsi untuk menghitung luas segitiga
+function hitungLuas() {
+    const tinggi = parseFloat(document.getElementById('input-tinggi-alas-luas').value);
+    const alas = parseFloat(document.getElementById('input-alas-luas').value);
+    
+    const luas = 0.5 * alas * tinggi;
+    
+    alert(`Luas segitiga adalah: ${luas}`);
+}
+
+// Fungsi untuk menghitung keliling segitiga
+function hitungKeliling() {
+    const sisi1 = parseFloat(document.getElementById('input-sisi1-keliling').value);
+    const sisi2 = parseFloat(document.getElementById('input-sisi2-keliling').value);
+    const sisi3 = parseFloat(document.getElementById('input-sisi3-keliling').value);
+    
+    const keliling = sisi1 + sisi2 + sisi3;
+    
+    alert(`Keliling segitiga adalah: ${keliling}`);
+}
